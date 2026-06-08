b591a7ee3e5e63374576c28b690b0468dc57b19b
LucivalMouraPena <164916375+LucivalMouraPena@users.noreply.github.com>
2026-06-08 10:14:50 -0300

    Primeiro commit Aula sobre FastAPI

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+vendas = {
+    1: {"item": "lata", "preco_unitario": 4, "quantidade": 5},
+    2: {"item": "garrafa 2L", "preco_unitario": 15, "quantidade": 5},
+    3: {"item": "garrafa 750ml", "preco_unitario": 10, "quantidade": 5},
+    4: {"item": "lata mini", "preco_unitario": 2, "quantidade": 5},
+}
+
+@app.get("/")
+def home():
+    return {"Vendas": len(vendas)}
+
+@app.get("/vendas/{id_venda}")
+def pegar_venda(id_venda: int):
+    if id_venda in vendas:
+        return vendas[id_venda]
+    else:
+        return {"Erro": "ID Venda inexistente"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
